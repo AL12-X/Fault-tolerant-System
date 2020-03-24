4ce21720a005b8eb166105042bdbb89f694861a8
SandalovKY <sandaloff.constantin@yandex.ru>
2020-03-24 19:21:27 +0400

    fixed merge conflict

--- operations/main.hpp
+++ operations/main.hpp
@@ -54,20 +54,6 @@ namespace FT {
 
     // NOT WORKING =======================================
     template<typename T>
-<<<<<<< HEAD
-    T land(const T& op1, const T& op2, ErrorStatus& err) {
-        T cnj1 = (op1) & (op2);
-        T tmp1 = (op1) & (~(op2));
-        tmp1 = tmp1 | cnj1;
-        tmp1 = tmp1 ^ op1;
-
-        if(tmp1 != 0) {
-            err = ErrorStatus::caughtError;
-            return ((cnj1 ^ tmp1));
-        }
-        err = ErrorStatus::noError;
-        return cnj1;
-=======
     T lAnd(const T& op1, const T& op2, ErrorStatus& err) {
         T res = op1 & op2; // checked and corrected
         T tmp = ((op1 & ~op2) | res) ^ op1;
@@ -77,6 +63,5 @@ namespace FT {
         }
         err = ErrorStatus::noError;
         return res;
->>>>>>> 595d334b53d1ea3465b06a89d8f4778696c4cb9c
     }
 }
